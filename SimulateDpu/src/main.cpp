93d4fd516f0b46ea8a83979c9a2e6bfaa1cfbeee
mechro-tang <1165100658@qq.com>
2024-03-04 17:30:03 +0800

    添加jsoncpp

--- SimulateDpu/src/main.cpp
+++ SimulateDpu/src/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include "UserInterface/HomePage.h"
+#include "json/json.h"
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello world!" << std::endl;
     
-    HomePage homePage = HomePage();
-    homePage.show();
+    //HomePage homePage = HomePage();
+    //homePage.show();
+
+    Json::Value root;
+    root["name"] = "John";
+    root["age"] = "30";
+
+    Json::StyledWriter writer;
+    
+    std::cout << writer.write(root) << std::endl;
     
     return 0;
 }
